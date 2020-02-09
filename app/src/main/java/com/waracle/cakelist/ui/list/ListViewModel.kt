9419b52ad94fc5f993882683556f23b6165f9439
Phil Bayfield <mail@phil.io>
2020-02-09 14:56:58 +0000

    Reworked view model to allow data to be refreshed

--- app/src/main/java/com/waracle/cakelist/ui/list/ListViewModel.kt
+++ app/src/main/java/com/waracle/cakelist/ui/list/ListViewModel.kt
@@ -1,20 +1,29 @@
 package com.waracle.cakelist.ui.list
 
-import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.liveData
+import androidx.lifecycle.viewModelScope
 import com.waracle.cakelist.data.model.Cake
 import com.waracle.cakelist.data.repository.CakeRepository
+import kotlinx.coroutines.launch
 
 class ListViewModel(private val cakeRepository: CakeRepository) : ViewModel() {
 
-    val cakes: LiveData<Result> = liveData {
-        emit(Result.Loading)
-        try {
-            val cakes = cakeRepository.getUniqueCakes().sortedBy { it.title }
-            emit(Result.Success(cakes))
-        } catch (exception: Exception) {
-            emit(Result.Error(exception))
+    val cakes = MutableLiveData<Result>()
+
+    init {
+        refresh()
+    }
+
+    fun refresh() {
+        cakes.value = Result.Loading
+        viewModelScope.launch {
+            try {
+                val newCakes = cakeRepository.getUniqueCakes().sortedBy { it.title }
+                cakes.postValue(Result.Success(newCakes))
+            } catch (exception: Exception) {
+                cakes.postValue(Result.Error(exception))
+            }
         }
     }
 }
--- app/src/test/java/com/waracle/cakelist/ui/list/ListViewModelTest.kt
+++ app/src/test/java/com/waracle/cakelist/ui/list/ListViewModelTest.kt
@@ -1,18 +1,14 @@
 package com.waracle.cakelist.ui.list
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.lifecycle.asFlow
 import com.waracle.cakelist.data.model.Cake
 import com.waracle.cakelist.data.repository.CakeRepository
 import com.waracle.cakelist.util.MainCoroutineRule
 import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.every
 import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.async
-import kotlinx.coroutines.flow.take
-import kotlinx.coroutines.flow.toList
-import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
@@ -24,40 +20,55 @@ class ListViewModelTest {
     val instantTaskExecutorRule = InstantTaskExecutorRule()
 
     @get:Rule
-    val coroutineTestRule = MainCoroutineRule()
+    val mainCoroutineRule = MainCoroutineRule()
 
     private val cakeRepository: CakeRepository = mockk()
 
     @Test
-    fun `Given repo returns cakes When view model is instantiated Then success is emitted and cakes are sorted by title alphabetically`() =
-        runBlockingTest {
-            val data = listOf(bakeCake("C"), bakeCake("A"), bakeCake("K"), bakeCake("E"), bakeCake("S"))
-            coEvery { cakeRepository.getUniqueCakes() } returns data
+    fun `Given repo returns cakes When view model is instantiated Then success is emitted and cakes are sorted by title alphabetically`() {
+        val data = listOf(bakeCake("C"), bakeCake("A"), bakeCake("K"), bakeCake("E"), bakeCake("S"))
+        coEvery { cakeRepository.getUniqueCakes() } returns data
 
-            coroutineTestRule.pauseDispatcher()
-            val viewModel = ListViewModel(cakeRepository)
-            val collection = async { viewModel.cakes.asFlow().take(2).toList() }
-            coroutineTestRule.runCurrent()
+        mainCoroutineRule.pauseDispatcher()
+        val viewModel = ListViewModel(cakeRepository)
+        assertEquals(Result.Loading, viewModel.cakes.value)
 
-            assertEquals(
-                listOf(Result.Loading, Result.Success(data.sortedBy { it.title })),
-                collection.await()
-            )
-        }
+        mainCoroutineRule.resumeDispatcher()
+        coVerify(exactly = 1) { cakeRepository.getUniqueCakes() }
+        assertEquals(Result.Success(data.sortedBy { it.title }), viewModel.cakes.value)
+    }
 
     @Test
-    fun `Given repo throws an error When view model is instantiated Then error is emitted`() =
-        runBlockingTest {
-            val exception = Exception("Something went wrong")
-            coEvery { cakeRepository.getUniqueCakes() } throws exception
+    fun `Given repo throws an error When view model is instantiated Then error is emitted`() {
+        val exception = Exception("Something went wrong")
+        coEvery { cakeRepository.getUniqueCakes() } throws exception
 
-            coroutineTestRule.pauseDispatcher()
-            val viewModel = ListViewModel(cakeRepository)
-            val collection = async { viewModel.cakes.asFlow().take(2).toList() }
-            coroutineTestRule.runCurrent()
+        mainCoroutineRule.pauseDispatcher()
+        val viewModel = ListViewModel(cakeRepository)
+        assertEquals(Result.Loading, viewModel.cakes.value)
 
-            assertEquals(listOf(Result.Loading, Result.Error(exception)), collection.await())
-        }
+        mainCoroutineRule.resumeDispatcher()
+        coVerify(exactly = 1) { cakeRepository.getUniqueCakes() }
+        assertEquals(Result.Error(exception), viewModel.cakes.value)
+    }
+
+    @Test
+    fun `Given repo returns new cakes When refresh is called Then success is emitted with new cakes`() {
+        val first = listOf(bakeCake("C"), bakeCake("A"))
+        val second = listOf(bakeCake("K"), bakeCake("E"))
+        coEvery { cakeRepository.getUniqueCakes() } returns first andThen second
+
+        val viewModel = ListViewModel(cakeRepository)
+        assertEquals(Result.Success(first.sortedBy { it.title }), viewModel.cakes.value)
+
+        mainCoroutineRule.pauseDispatcher()
+        viewModel.refresh()
+        assertEquals(Result.Loading, viewModel.cakes.value)
+
+        mainCoroutineRule.resumeDispatcher()
+        coVerify(exactly = 2) { cakeRepository.getUniqueCakes() }
+        assertEquals(Result.Success(second.sortedBy { it.title }), viewModel.cakes.value)
+    }
 
     private fun bakeCake(title: String): Cake {
         val cake = mockk<Cake>()
